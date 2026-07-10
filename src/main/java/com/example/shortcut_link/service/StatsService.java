b4d462bef4a7d8be521923d19efdc849fa818856
Mikitos06 <vojdaknikita@gmail.com>
2026-07-10 14:25:12 +0300

    Add StatsService

---
+++ src/main/java/com/example/shortcut_link/service/StatsService.java
@@ -0,0 +1,40 @@
+package com.example.shortcut_link.service;
+
+import com.example.shortcut_link.entity.Link;
+import com.example.shortcut_link.entity.Stats;
+import com.example.shortcut_link.exception.NotFoundException;
+import com.example.shortcut_link.repository.StatsRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class StatsService {
+
+    private final StatsRepository statsRepository;
+
+    public StatsService(StatsRepository statsRepository) {
+        this.statsRepository = statsRepository;
+    }
+
+    @Transactional
+    public void createStatsForLink(Link link) {
+        Stats stats = new Stats(link);
+        statsRepository.save(stats);
+    }
+
+    @Transactional
+    public void incrementClickCount(Link link) {
+        Stats stats = statsRepository.findByLinkId(link.getId())
+        .orElseThrow(() -> new NotFoundException("Statistics for the link not found."));
+        stats.incrementClick();
+        statsRepository.save(stats);
+    }
+
+    @Transactional(readOnly = true)
+    public Long getClickCount(Link link) {
+        return statsRepository.findByLinkId(link.getId())
+                .map(Stats::getClickCount)
+                .orElse(0L);
+    }
+}
