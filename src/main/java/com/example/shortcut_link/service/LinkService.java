21803e9f831527e64b2b5fcf6896a3c33155dc07
Mikitos06 <vojdaknikita@gmail.com>
2026-07-10 14:02:13 +0300

    Add simple service

---
+++ src/main/java/com/example/shortcut_link/service/LinkService.java
@@ -0,0 +1,50 @@
+package com.example.shortcut_link.service;
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.example.shortcut_link.repository.LinkRepository;
+import com.example.shortcut_link.entity.Link;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+@Service
+public class LinkService {
+
+    private final LinkRepository linkRepository;
+
+    public LinkService(LinkRepository linkRepository){
+        this.linkRepository = linkRepository;
+    }
+
+    @Transactional
+    public int createLink(String originalURL, String shortCode, LocalDateTime expiresAt) {
+        Link link = new Link();
+        link.setShortCode(shortCode);
+        link.setOriginalURL(originalURL);
+        link.setExpiresAt(expiresAt);
+        link = linkRepository.save(link);
+        return link.getId();
+    }
+
+    @Transactional
+    public int createLink(String originalURL,LocalDateTime expiresAt) {
+        Link link = new Link();
+        link.setShortCode(generateShortCode());
+        link.setOriginalURL(originalURL);
+        link.setExpiresAt(expiresAt);
+        link = linkRepository.save(link);
+        return link.getId();
+    }
+
+    private String generateShortCode() {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(20);
+        for (int i = 0; i < 20; i++) {
+            sb.append(chars.charAt(random.nextInt(chars.length())));
+        }
+        return sb.toString();
+    }
+}
