82663f73bf540655de0b097e94b3992415903e56
Mikitos06 <vojdaknikita@gmail.com>
2026-07-10 13:23:26 +0300

    edit Link

--- src/main/java/com/example/shortcut_link/entity/Link.java
+++ src/main/java/com/example/shortcut_link/entity/Link.java
@@ -1,5 +1,7 @@
 package com.example.shortcut_link.entity;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.*;
 @Entity
 @Table(name = "links")
@@ -14,6 +16,15 @@ public class Link {
     @Column (name = "short_code", nullable = false, length = 20)
     private String shortCode;
 
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "expires_at")
+    private LocalDateTime expiresAt;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive = true;
+
     public Integer getId() { return id; }
 
     public String getOriginalURL() { return originalURL; }
@@ -21,5 +32,15 @@ public class Link {
 
     public String getShortCode() { return shortCode; }
     public void setShortCode(String shortCode) { this.shortCode = shortCode; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+
+    public LocalDateTime getExpiresAt() { return expiresAt; }
+    public void setExpiresAt(LocalDateTime expiresAt) { this.expiresAt = expiresAt; }
+
+    public Boolean IsActive() { return isActive; }
+    public void setIsActive(Boolean isActive) { this.isActive = isActive; }
+
 }
 
