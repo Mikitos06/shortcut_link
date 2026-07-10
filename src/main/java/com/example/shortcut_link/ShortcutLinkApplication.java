967bb070676eda3f0c1bd4416eea7a74a0f902eb
Mikitos06 <vojdaknikita@gmail.com>
2026-07-10 12:35:24 +0300

    Init commit

---
+++ src/main/java/com/example/shortcut_link/ShortcutLinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.shortcut_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortcutLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortcutLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shortcut_link/ShortcutLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shortcut_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortcutLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
